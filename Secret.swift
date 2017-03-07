704fab9708fe1361ca790fd1d5582f962aefd8a5
Oliver Plunkett <otplunkett@gmail.com>
2017-03-07 10:47:43 -0500

    fix case

--- Secret.swift
+++ Secret.swift
@@ -1,14 +1,3 @@
-//
-//  Secret.swift
-//  Track Your Reps
-//
-//  Created by Benjamin Bernstein on 3/6/17.
-//  Copyright © 2017 Burning Flowers. All rights reserved.
-//
-
-import Foundation
-
-enum Secret {
-    static let key = "04VcutIuRh2JR6XjLRzVg2vwFpKe5vKXp4iuI0Yd"
-
+enum Secrets: String {
+    case proPublicaApiKey = "04VcutIuRh2JR6XjLRzVg2vwFpKe5vKXp4iuI0Yd"
 }
