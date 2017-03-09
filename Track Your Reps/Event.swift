1512ef02391e63224bcfab0aae891a793540663a
Ben Bernstein <bb@burningflowe.rs>
2017-03-09 14:18:41 -0500

    events in feed v1

--- Track Your Reps/Event.swift
+++ Track Your Reps/Event.swift
@@ -8,20 +8,35 @@ import SwiftyJSON
 struct Event {
     
     var bill: String?
-    var date: String?
-    var description: String?
-    var position: String?
-    var question: String?
-    var repId: String?
+    var rawDate: String
+    var description: String
+    var position: String
+    var question: String
+    var repId: String
+    var repName: String
     var time: String?
     
     init(from json: JSON, for member: CongressMember) {
         self.bill = json["bill"].stringValue
-        self.date = json["date"].stringValue
+        self.rawDate = json["date"].stringValue
         self.description = json["description"].stringValue
         self.position = json["position"].stringValue
         self.question  = json["question"].stringValue
-        self.repId = member.id
         self.time = json["time"].stringValue
+        
+        self.repName = member.fullName
+        self.repId = member.id
+      }
+}
+
+extension Event: Hashable {
+    
+    var hashValue: Int {
+        return description.hashValue
     }
+    
+    static func ==(lhs: Event, rhs: Event) -> Bool {
+        return lhs.description == rhs.description
+    }
+    
 }
