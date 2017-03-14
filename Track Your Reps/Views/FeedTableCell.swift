6060f76b130878e9640923257012a8c1f5e044bd
Ben Bernstein <bb@burningflowe.rs>
2017-03-13 22:57:09 -0400

    bill data, timeSince, event labels

--- Track Your Reps/Views/FeedTableCell.swift
+++ Track Your Reps/Views/FeedTableCell.swift
@@ -6,22 +6,28 @@ import UIKit
 
 class FeedTableCell: UITableViewCell {
     
-    let eventQuestionLabel = UILabel()
+    let eventQuestionLabel = UITextView()
     let eventTimeLabel = UILabel()
     let eventDescriptionLabel = UILabel()
     let repActionLabel = UILabel()
     
     var event: Event? {
         didSet {
-            eventQuestionLabel.text = "\(event!.isBill ? "I'm a bill! - " : "")\(event?.question)"
-            eventDescriptionLabel.text = event?.eventDescription
-            repActionLabel.attributedText = event?.memberPositions
-            eventTimeLabel.text = event?.date
+            guard let event = event else { return }
+            repActionLabel.attributedText = event.memberPositions
+            eventTimeLabel.text = formatDate(event.date)
+            switch event.isBill {
+            case true:
+                setUpBill(event)
+            case false:
+                setUpNonBill(event)
+            }
+            
         }
     }
     
-    var leftLabels: [UILabel] {
-        return [eventQuestionLabel, eventDescriptionLabel, repActionLabel]
+    var labels: [UILabel] {
+        return [eventDescriptionLabel, repActionLabel]
     }
     
     static let reuseID = "events"
@@ -44,52 +50,118 @@ class FeedTableCell: UITableViewCell {
 // MARK: - Layout
 
 extension FeedTableCell {
-
     
     func setupView() {
-        leftLabels.forEach { contentView.addSubview($0) }
+        labels.forEach { contentView.addSubview($0) }
+        contentView.addSubview(eventQuestionLabel)
         contentView.addSubview(eventTimeLabel)
         setupLabels()
     }
     
     func setupLabels() {
-        eventQuestionLabel.font = UIFont(name: "Montserrat-Light", size: 14)
-        eventQuestionLabel.textColor = Palette.grey.color
+        eventQuestionLabel.font = UIFont(name: "Montserrat-Regular", size: 14)
+        eventQuestionLabel.textColor =  Palette.white.color
+        eventQuestionLabel.backgroundColor = Palette.black.color
         
-        eventTimeLabel.font = UIFont(name: "Montserrat-Light", size: 12)
+        // TODO: calculate this more effectively without breaking constraints
+        eventQuestionLabel.textContainerInset = UIEdgeInsetsMake(5, 30, 5, 10)
+        eventQuestionLabel.textAlignment = .left
+        
+        eventTimeLabel.font = UIFont(name: "Montserrat-Light", size: 14)
         eventTimeLabel.textColor = Palette.grey.color
         
-        eventDescriptionLabel.font = UIFont(name: "Montserrat-Light", size: 14)
-        repActionLabel.font = UIFont(name: "Montserrat-Regular", size: 12)
+        eventDescriptionLabel.font = UIFont(name: "Montserrat-Regular", size: 16)
+        eventDescriptionLabel.setLineHeight(lineHeight: 10)
+        
+        repActionLabel.font = UIFont(name: "Montserrat-Regular", size: 14)
+        repActionLabel.attributedText = event?.memberPositions
         
         setupConstraints()
     }
     
+    
     func setupConstraints() {
-        leftLabels.forEach { setupCommonConstraints($0) }
+        labels.forEach { setupCommonConstraints($0) }
         eventTimeLabel.translatesAutoresizingMaskIntoConstraints = false
         eventTimeLabel.numberOfLines = 0
+        eventQuestionLabel.translatesAutoresizingMaskIntoConstraints = false
+        
         setupUniqueConstraints()
     }
     
     func setupCommonConstraints(_ label: UILabel) {
         label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.leadingAnchor.constraint(equalTo: marginsGuide.leadingAnchor, constant: contentView.frame.width * 0.05).isActive = true
+        
     }
     
     func setupUniqueConstraints() {
         
         eventQuestionLabel.topAnchor.constraint(equalTo: marginsGuide.topAnchor, constant: contentView.frame.height * 0.3).isActive = true
-        eventQuestionLabel.trailingAnchor.constraint(equalTo: marginsGuide.trailingAnchor).isActive = true
+        eventQuestionLabel.widthAnchor.constraint(lessThanOrEqualTo: marginsGuide.widthAnchor, multiplier: 0.8).isActive = true
+        eventQuestionLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        eventQuestionLabel.isScrollEnabled = false
+        eventQuestionLabel.translatesAutoresizingMaskIntoConstraints = false
         
         eventTimeLabel.topAnchor.constraint(equalTo: eventQuestionLabel.topAnchor).isActive = true
         eventTimeLabel.trailingAnchor.constraint(equalTo: marginsGuide.trailingAnchor).isActive = true
         
-        eventDescriptionLabel.topAnchor.constraint(equalTo: eventQuestionLabel.bottomAnchor, constant: contentView.frame.height * 0.1).isActive = true
+        eventDescriptionLabel.topAnchor.constraint(equalTo: eventQuestionLabel.bottomAnchor, constant: contentView.frame.height * 0.4).isActive = true
         eventDescriptionLabel.trailingAnchor.constraint(equalTo: marginsGuide.trailingAnchor, constant: -contentView.frame.width * 0.1).isActive = true
+        eventDescriptionLabel.leadingAnchor.constraint(equalTo: marginsGuide.leadingAnchor, constant: contentView.frame.width * 0.05).isActive = true
         
         repActionLabel.topAnchor.constraint(equalTo: eventDescriptionLabel.bottomAnchor, constant: contentView.frame.height * 0.3).isActive = true
+        repActionLabel.trailingAnchor.constraint(equalTo: marginsGuide.trailingAnchor).isActive = true
         repActionLabel.bottomAnchor.constraint(equalTo: marginsGuide.bottomAnchor).isActive = true
     }
+    
+    func formatDate(_ date: String) -> String {
+        
+        let dateFormatterGet = DateFormatter()
+        dateFormatterGet.dateFormat = "yyyy-MM-dd"
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM d"
+        
+        let date: Date? = dateFormatterGet.date(from: date)
+        
+        return dateFormatter.timeSince(from: date!)
+    }
 }
+
+// MARK: - Bill or NonBill Specific Layout
+
+extension FeedTableCell {
+    
+    func setUpBill(_ event: Event) {
+        eventQuestionLabel.text = event.bill?.subject.uppercased()
+        guard let safeBill = event.bill else { return }
+        eventQuestionLabel.backgroundColor = determineBackgroundColor(safeBill.subject)
+        
+        if event.bill?.summary != "" {
+            eventDescriptionLabel.text = event.bill?.summary.replacingOccurrences(of: "(This measure has not been amended since it was introduced. The summary of that version is repeated here.) ", with: "").replacingOccurrences(of: "&quot;", with: "\"")
+        } else {
+            eventDescriptionLabel.text = event.eventDescription
+        }
+    
+    }
+    
+    func determineBackgroundColor(_ text:String) -> UIColor {
+        
+        if text.contains("Education") {
+            return Palette.green.color
+        } else if text.contains("Natural") {
+            return Palette.blue.color
+        } else {
+            return Palette.black.color
+        }
+        
+    }
+    
+    func setUpNonBill(_ event: Event) {
+        eventQuestionLabel.text = event.question.uppercased()
+        eventDescriptionLabel.text = event.eventDescription
+       
+    }
+}
+
